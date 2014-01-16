91a45a67702ff6cbf96b2a994ae569a7b1809a03
Simon Engelhardt <simon@engelhardt.it>
2014-01-16 00:17:41 -0500

    Initial commit

---
+++ server/api.js
@@ -0,0 +1,32 @@
+Meteor.Router.add('/api/handle_push', function() {
+	var secret = this.request.body.secret;
+
+	if (secret != "***REMOVED***") {
+		return [403, 'Invalid secret provided'];
+	}
+
+	var checkin = JSON.parse(this.request.body.checkin);
+
+	var user = Meteor.users.findOne({"services.foursquare.id": checkin.user.id });
+
+	if (user) {
+		var location = checkin.venue.location;
+
+		var result = HTTP.call(
+			"GET", 
+			"https://api.forecast.io/forecast/***REMOVED***/" + location.lat + "," + location.lng,
+			function(error, result){
+				if (result.statusCode === 200) {
+					HTTP.call(
+						"POST", 
+						"https://api.foursquare.com/v2/checkins/" + checkin.id + "/reply",
+						{params: {
+							oauth_token: user.services.foursquare.accessToken,
+							text: result.data.minutely.summary
+						}}
+					);
+				}
+			}
+		);
+	}
+});
