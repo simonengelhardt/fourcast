174c9d2dd5f63111285e6b9f714efa54cdea09c7
Simon Engelhardt <simon@engelhardt.io>
2014-05-26 19:09:48 -0400

    Removed leftover content from hello world sample
    And a little bit of text cleanup

--- fourcast.js
+++ fourcast.js
@@ -1,15 +1,7 @@
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Welcome to fourcast.";
+    return "Welcome to Fourcast.";
   };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 }
 
 if (Meteor.isServer) {
